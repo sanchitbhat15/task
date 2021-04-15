ea691aa615685eeeaf882acfa271bec96af71fea
sanchitbhat15 <bhat.sanchit@gmail.com>
2021-04-15 17:47:23 +0530

    local added

---
+++ Local.js
@@ -0,0 +1,10 @@
+
+localStorage.setItem('Name', 'Sanchit');
+localStorage.setItem('Name2', 'Bhat');
+
+let name = localStorage.getItem('Name');
+console.log(name);
+
+let name2 = localStorage.getItem('Name2');
+console.log(name2);
+
