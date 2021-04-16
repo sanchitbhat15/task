a4cc3353f9961ebcc2bdadaa0d36fb3f5c3eb046
sanchitbhat15 <bhat.sanchit@gmail.com>
2021-04-16 18:59:34 +0530

    task2 added

---
+++ task2.js
@@ -0,0 +1,8 @@
+//Get element by class
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor = 'green'; //Make the 3 rd element in the list have green background color
+items[0].style.fontWeight = 'bold'; 
+items[1].style.fontWeight = 'bold'; 
+items[2].style.fontWeight = 'bold'; 
+items[3].style.fontWeight = 'bold'; 
