0137f9413b0e37c0c7483a78a3a9fd65ad3bdf5c
sanchitbhat15 <bhat.sanchit@gmail.com>
2021-05-29 22:11:46 +0530

    main js file added

---
+++ serving html css js/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact');
+const successRoutes = require('./routes/success');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
+app.use((req,res,next)=> {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+
+});
+
+app.listen(4000);
