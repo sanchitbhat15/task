43727ebd8fe1671656384de02345de3de8c59a22
sanchitbhat15 <bhat.sanchit@gmail.com>
2021-05-29 17:02:36 +0530

    admin.js & shop.js added

---
+++ Routes and Filters/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product',(req,res,next) => {
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="Size"><button type= "submit">Add product</button></form>');
+   
+});
+
+router.post('/add-product',(req,res,next)=> {
+    console.log(req.body);
+    res.redirect('/shop/');
+});
+
+module.exports = router;
